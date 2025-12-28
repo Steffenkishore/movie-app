f5889db9cda8800c677e270933dd1249d9ea2043
Steffenkishore <kishoresteffen@gmail.com>
2025-12-28 23:55:32 +0530

    added render url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,3 @@
-// const BASE_URL = "http://localhost:5000/api/movies";
-
 export const getPopularMovies = async () => {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/popular`);
   return await res.json();
